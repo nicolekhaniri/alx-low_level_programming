00d8a4813d5e6e3b1ff3ecd8cd8a93817f67edb3
Nicole Khaniri <nicolekhaniri@gmail.com>
2022-09-11 13:41:02 -0700

    2. I sometimes suffer from insomnia.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stddio.h>
+/**
+* main - Write a program that prints the alphabet in lowercase, followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
