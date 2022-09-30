c7443c10bfdfdbe4cfb8f87d4ba41ebf3d9d87ba
nicolekhaniri <nicolekhaniri@gmail.com>
2022-09-30 09:59:15 -0700

    3. Neither irony nor sarcasm is argument fixed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,10 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - prints multiples of two numbers
+ * @argc: length of the arguments in total
+ * @argv: the arguments themselves
+ *
+ * Return: 1 if the arguments exceed 3 and 0 if success
+ *
+ */
+
 int main(int argc, char *argv[])
 {
 	int i, mult;
+
 	mult = 1;
+
 	if (argc < 3)
 	{
 		printf("Error\n");
