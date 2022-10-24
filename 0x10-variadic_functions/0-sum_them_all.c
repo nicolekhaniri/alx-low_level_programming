5d6155d742d70d077c0deebbb0607f883d892750
nicolekhaniri <nicolekhaniri@gmail.com>
2022-10-24 13:05:07 -0700

    Betty compliant 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,6 +3,7 @@
 /**
  * sum_them_all - returns the sum of parameters
  * @n: amount of arguments
+ * Return: sum of parameters
  */
 
 int sum_them_all(const unsigned int n, ...)
@@ -11,11 +12,11 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 
-	if(n == 0)
+	if (n == 0)
 	{
 		return (0);
 	}
-	
+
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
@@ -24,5 +25,5 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	va_end(valist);
 
-	return(sum);
+	return (sum);
 }
