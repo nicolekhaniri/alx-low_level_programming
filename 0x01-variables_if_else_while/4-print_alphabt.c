895c3501229ac8591d5df49e83a37f3f8af6c0bb
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2022-12-18 15:38:09 +0300

    removing warnings

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - write a program that prints the alphabet in lowercase except q and e
- * 
+ *
  * Description - use main function
  *
  * Return: 0
@@ -9,6 +9,7 @@
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 	if (ch != 'e' && ch != 'q')
