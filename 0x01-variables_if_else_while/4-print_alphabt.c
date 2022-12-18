9e5603978e99d285f8ae5a5a43f1f17f856a3452
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2022-12-18 15:36:28 +0300

    removing whitespaces

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
- * main - write a program that prints the alphabel in lowercase followed by a new line
+ * main - write a program that prints the alphabet in lowercase except q and e
  * 
  * Description - use main function
- * print all the letters except q and e
+ *
  * Return: 0
  */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-if (ch != 'e' && ch != 'q')
-{
-	putchar(ch);
-}
-}
-putchar('\n');
-return (0);
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	if (ch != 'e' && ch != 'q')
+	{
+		putchar(ch);
+	}
+	}
+	putchar('\n');
+	return (0);
 }
