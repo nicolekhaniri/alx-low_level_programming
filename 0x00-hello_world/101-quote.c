eb6d24197c0f94689decf367070b1c30dd7d42d5
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-18 04:05:01 -0800

    trying to create the correct output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(STDERR_FILENO, str, sizeof(str - 1));
+	write(STDERR_FILENO, str, sizeof(str) - 1);
 	return (1);
 }
