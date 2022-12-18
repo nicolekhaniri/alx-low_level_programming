e22d7086c0f1abeb0ae8c053dfcfa70f58b5b423
nicolekhaniri <49895563+nicolekhaniri@users.noreply.github.com>
2022-12-18 14:37:42 +0300

    advanced tasks not executed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+/**
+ * main - program that prints something
+ *
+ * Description: prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *
+ * Returns: 1
+ */
+int main(void){
+	char str[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+		write(STDERR_FILENO, str, sizeof str - 1);
+	return (1);
+}
