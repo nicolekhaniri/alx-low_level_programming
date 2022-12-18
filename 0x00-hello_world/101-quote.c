dad1eeb44be0fb97dc34e854cc200e25c7b56ca4
nicolekhaniri <nicolekhaniri@gmail.com>
2022-12-18 03:54:46 -0800

    another trial

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <unistd.h>
-
 /**
  * main - program that prints something
  *
- * Description: prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
- *
  * Return: 1
  */
-int main(void){
-	char str[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	write(STDERR_FILENO, str, sizeof str - 1);
 	return (1);
 }
